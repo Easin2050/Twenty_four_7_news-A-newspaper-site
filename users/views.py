9b81896d6a4695b74dcefa8fefd9d0ab7d421ec9
easin <easin562050@gmail.com>
2025-10-07 00:27:03 +0600

    error fixed

--- users/views.py
+++ users/views.py
@@ -11,20 +11,12 @@ from rest_framework.filters import SearchFilter,OrderingFilter
 from rest_framework.mixins import CreateModelMixin,RetrieveModelMixin,UpdateModelMixin
 from api.permissions import IsProfileOwner
 from drf_yasg.utils import swagger_auto_schema
-
-
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
-from django.contrib.auth import get_user_model
-from rest_framework.viewsets import ModelViewSet
-from .serializers import UserSerializer
-from .pagination import CustomPagination
+from .signals import create_or_update_user_profile
 
 class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
     pagination_class = CustomPagination
-    http_method_names = ["get", "put", "patch"]  
+    http_method_names = ["get", "put", "patch"]
 
     def get_queryset(self):
         if self.request.user.is_superuser:
@@ -32,24 +24,11 @@ class UserViewSet(ModelViewSet):
         return get_user_model().objects.filter(id=self.request.user.id)
 
     def get_permissions(self):
-        if self.request.method in ["GET", "PUT", "PATCH","POST"]:
+        if self.request.method in ["GET", "PUT", "PATCH"]:
             return [IsAuthenticated()]
         return [IsAdminUser()]
-
-    def create(self, request, *args, **kwargs):
-        """
-        Override create() to return only a success message after registration.
-        """
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        return Response(
-            {"message": "Account created successfully. Please verify your email if required."},
-            status=status.HTTP_201_CREATED
-        )
-
-
     
+
 class UserProfileViewSet(GenericViewSet, RetrieveModelMixin, CreateModelMixin, UpdateModelMixin):
     serializer_class = UserProfileSerializer
     permission_classes = [IsAuthenticated, IsProfileOwner]
