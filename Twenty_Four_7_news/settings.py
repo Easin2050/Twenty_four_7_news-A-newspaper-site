6ff7703e725cb48f0d5863f5147038b888f69dac
easin <easin562050@gmail.com>
2025-10-09 22:33:55 +0600

    whitenoise configuration done

--- Twenty_Four_7_news/settings.py
+++ Twenty_Four_7_news/settings.py
@@ -27,14 +27,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-id-66j%bw@%h%zh)5+wu$w@0m2ynm2rc2+=$9g7k)-a07ycv+='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app,127.0.0.1']
 
 AUTH_USER_MODEL='users.User'
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -52,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -161,8 +163,11 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE="whitenoise.storage.CompressedStaticFilesStorage"
 MEDIA_URL='/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
 
 
 REST_FRAMEWORK = {
